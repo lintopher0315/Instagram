4c24439e97084b5bb77e365ba2713c89690ac630
Christopher Lin <christopherlin@Christophers-iMac.local>
2019-02-16 15:31:34 -0700

    init commit

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Christopher Lin on 2/16/19.
+//  Copyright © 2019 Christopher Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
