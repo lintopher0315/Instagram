aa23b0ff4dc8101b0512f11c89e82ee21adf41d2
Christopher Lin <christopherlin@Christophers-iMac.local>
2019-02-25 21:18:24 -0700

    logging out

--- Instagram/Instagram/FeedViewController.swift
+++ Instagram/Instagram/FeedViewController.swift
@@ -78,4 +78,13 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     */
 
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        delegate.window?.rootViewController = loginViewController
+    }
 }
