c4cc37e69a99e0179dbc99d25c4759ff8b169738
Christopher Lin <christopherlin@Christophers-iMac.local>
2019-02-25 21:10:40 -0700

    added feednavigationcontroller

--- Instagram/Instagram/AppDelegate.swift
+++ Instagram/Instagram/AppDelegate.swift
@@ -25,6 +25,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             })
         )
         
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            
+            window?.rootViewController = feedNavigationController
+        }
+        
         return true
     }
 
